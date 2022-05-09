adf74832306d68f6c53c6c45f6445c6b1d2a3942
Bosn_Fox <bogdanovplaton@gmail.com>
2022-05-09 13:27:18 +0300

    Список профессий

--- main.py
+++ main.py
@@ -30,5 +30,48 @@ def training(prof):
                 </html>"""
 
 
+@app.route('/list_prof/<list>')
+def listing(liste):
+    try:
+        assert liste in ['ul', 'ol']
+        return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+                    crossorigin="anonymous">
+                    <title>Колонизация Марса</title>
+                  </head>
+                  <body>
+                    <h2>Cписок работ</h2>
+                    <ul>
+                    <{liste}>Вилкой чистить</{liste}>
+                    <{liste}>Рисовать четвёртый сезон Вакфу</{liste}>
+                    <{liste}>Молиться Космосу ради еды</{liste}>
+                    <{liste}>Записывать летсплеи по ENA</{liste}>
+                    </ul>
+                  </body>
+                </html>"""
+    except AssertionError:
+        return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+                    crossorigin="anonymous">
+                    <title>Колонизация Марса</title>
+                  </head>
+                  <body>
+                    <div class="alert alert-danger">Некорректный ввод типа списка</div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
